5315c1e48ad812417968d1118984cdadea17cd57
Phil Newman <pnewman@viimed.com>
2019-10-27 02:46:01 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,21 @@
-def addition(num1, num2)
-
+def addition(num1="4", num2="5")
+  num1 + num2
 end
 
-def subtraction(num1, num2)
-
+def subtraction(num1="10", num2="5")
+  num1 - num2
 end
 
-def division(num1, num2)
-
+def division(num1="50", num2="2")
+  num1 / num2
 end
 
-def multiplication(num1, num2)
-
+def multiplication(num1="4", num2="30")
+  num1 * num2
 end
 
-def modulo(num1, num2)
-
+def modulo(num1="34", num2="5")
+  puts #{num1}.modulo(#{num2})
 end
 
 def square_root(num)
